fb68115dc045325339665634789100ec287630a1
LuccaMenezes <luccamen2016@gmail.com>
2023-06-05 00:02:22 -0300

    created login function

--- frontend/src/hooks/useAuth.jsx
+++ frontend/src/hooks/useAuth.jsx
@@ -9,7 +9,7 @@ export default function useAuth() {
    const [authenticated, setAuthenticated] = useState(false)
    const { setFlashMessage } = useFlashMessage()
    const navigate = useNavigate()
-   
+
    useEffect(() => {
       const token = localStorage.getItem('token')
 
@@ -30,8 +30,8 @@ export default function useAuth() {
          })
          await authUser(data)
       } catch (error) {
-          msgText = error.response.data.message
-          msgType = 'error'
+         msgText = error.response.data.message
+         msgType = 'error'
       }
 
       setFlashMessage(msgText, msgType)
@@ -58,6 +58,24 @@ export default function useAuth() {
 
       setFlashMessage(msgText, msgType)
    }
-   
-   return { authenticated, register, logout }
+
+   async function login(user) {
+      let msgText = 'Login realizado com sucesso!'
+      let msgType = 'success'
+
+      try {
+         const data = await api.post('/users/login', user).then((response) => {
+            return response.data
+         })
+
+         await authUser(data)
+      } catch (error) {
+         msgText = error.response.data.message
+         msgType = 'error'
+      }
+
+      setFlashMessage(msgText, msgType)
+   }
+
+   return { authenticated, register, logout, login }
 }
--- frontend/src/pages/login/Login.jsx
+++ frontend/src/pages/login/Login.jsx
@@ -1,8 +1,48 @@
-import React from 'react'
+import { useState, useContext } from 'react'
+import { Grid, Paper, Avatar, Typography, TextField, Button } from '@mui/material'
+import { Link } from 'react-router-dom';
+
+import { Context } from '../../context/UserContext'
 
 const login = () => {
+
+  const [user, setUser] = useState({})
+  const { login } = useContext(Context)
+
+  function handleChange(e) {
+    setUser({ ...user, [e.target.name]: e.target.value })
+    console.log(user);
+  }
+
+  function handleSubmit(e) {
+    e.preventDefault()
+    login(user)
+  }
+  const paperStyle = { padding: '30px 30px', width: 350, margin: '20px auto', borderRadius: '10px' }
+  const headerStyle = { margin: 0 }
+  const avatarStyle = { backgroundColor: '#1bbd7e' }
+
   return (
-    <div>login</div>
+    <div>
+      <Grid>
+        <Paper elevation={20} style={paperStyle}>
+          <Grid align='center'>
+            <Avatar style={avatarStyle}>
+
+            </Avatar>
+            <h2 style={headerStyle}>Login</h2>
+          </Grid>
+          <form onSubmit={handleSubmit}>
+            <TextField fullWidth label='Email' name="email" variant="standard" onChange={handleChange} />
+            <TextField fullWidth label='Senha' name="password" variant="standard" onChange={handleChange} />
+            <Button type='submit' variant='contained' color='primary'>Cadastrar</Button>
+          </form>
+          <Typography align='center'>
+            Não tem conta? <Link to="/register">Clique Aqui</Link>
+          </Typography>
+        </Paper>
+      </Grid>
+    </div>
   )
 }
 
