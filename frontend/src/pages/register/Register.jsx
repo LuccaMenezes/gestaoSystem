07c6b6d5af99b2d4433da5d73a8500aa59bb25bd
LuccaMenezes <luccamen2016@gmail.com>
2023-06-08 23:39:38 -0300

    Register page styling

--- frontend/src/pages/register/Register.jsx
+++ frontend/src/pages/register/Register.jsx
@@ -2,48 +2,78 @@ import { Grid, Paper, Avatar, Typography, TextField, Button } from '@mui/materia
 import AddCircleOutlineOutlinedIcon from '@mui/icons-material/AddCircleOutlineOutlined';
 import React, { useState, useContext } from 'react'
 
+import Logo from '../../assets/img/SimpleLogo.png'
+
 import { Context } from '../../context/UserContext'
 
 
 function Register() {
 
    const [user, setUser] = useState({})
-   const {register} = useContext(Context) 
+   const { register } = useContext(Context)
 
    function handleChange(e) {
-      setUser({ ...user, [e.target.name]: e.target.value})
+      setUser({ ...user, [e.target.name]: e.target.value })
       console.log('a ' + setUser);
    }
 
-   function handleSubmit (e) {
+   function handleSubmit(e) {
       e.preventDefault()
       register(user)
    }
 
-   const paperStyle={padding:'30px 30px', width:350, margin:'20px auto', borderRadius:'10px' }
-   const headerStyle={margin: 0}
-   const avatarStyle={backgroundColor: '#1bbd7e'}
+   const paperStyle = {
+      padding: '60px 30px',
+      width: 350,
+      margin: '20px auto',
+      borderRadius: '10px'
+   }
+
+   const logoStyle = {
+      width: '100px',
+      padding: '5px',
+      position: 'absolute',
+      top: '15px',
+      left: '50%',
+      transform: 'translateX(-50%)'
+   }
+
+   const containerStyle = {
+      display: 'flex',
+      justifyContent: 'center',
+      alignItems: 'center',
+      height: '100vh',
+      background: 'linear-gradient(90deg, rgba(230,230,230,1) 0%, rgba(255,255,255,1) 100%)'
+   }
+
+   const formStyle = {
+      display: 'flex',
+      flexDirection: 'column',
+   }
+
+   const buttonStyle = {
+      marginTop: '20px',
+      background: '#f6c71e'
+   }
 
    return (
-      <Grid>
-         <Paper elevation={20} style={paperStyle}>
-           <Grid align='center'>
-             <Avatar style={avatarStyle}>
-               
-             </Avatar>
-             <h2 style={headerStyle}>Cadastre-se</h2>
-           </Grid>
-           <form onSubmit={handleSubmit}>
-               <TextField fullWidth label='Name' name="name" variant="standard" onChange={handleChange}/>
-               <TextField fullWidth label='Email' name="email" variant="standard" onChange={handleChange}/>
-               <TextField fullWidth label='Senha' name="password" variant="standard" onChange={handleChange}/>
-               <TextField fullWidth label='Confirme sua senha' name="confirmpassword" variant="standard" onChange={handleChange}/>
-               <TextField fullWidth label='Telefone' name="phone" variant="standard" onChange={handleChange}/>
-               <TextField fullWidth label='Empresa' name="company" variant="standard" onChange={handleChange}/>
-               <Button type='submit' variant='contained' color='primary'>Cadastrar</Button>
-           </form>
-         </Paper>
-      </Grid>
+
+      <div style={containerStyle}>
+         <div>
+            <img src={Logo} alt="Gestão System" style={logoStyle} />
+            <Paper elevation={20} style={paperStyle}>
+               <form onSubmit={handleSubmit} style={formStyle}>
+                  <TextField fullWidth label='Nome' name="name" variant="standard" onChange={handleChange} />
+                  <TextField fullWidth label='Email' name="email" variant="standard" onChange={handleChange} style={{ marginTop: '10px' }} />
+                  <TextField fullWidth label='Senha' name="password" variant="standard" onChange={handleChange} style={{ marginTop: '10px' }} />
+                  <TextField fullWidth label='Confirme sua senha' name="confirmpassword" variant="standard" onChange={handleChange} style={{ marginTop: '10px' }} />
+                  <TextField fullWidth label='Telefone' name="phone" variant="standard" onChange={handleChange} style={{ marginTop: '10px' }} />
+                  <TextField fullWidth label='Empresa' name="company" variant="standard" onChange={handleChange} style={{ marginTop: '10px' }} />
+                  <Button type='submit' variant='contained' style={buttonStyle}>Cadastrar</Button>
+               </form>
+            </Paper>
+         </div>
+      </div>
    )
 }
 
