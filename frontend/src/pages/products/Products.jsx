7a1d9781f078bb665ff8676091560f001864b7bd
LuccaMenezes <luccamen2016@gmail.com>
2023-06-11 09:11:56 -0300

    Added pagination to browse the grid

--- frontend/src/pages/products/Products.jsx
+++ frontend/src/pages/products/Products.jsx
@@ -14,12 +14,26 @@ import TableHead from '@mui/material/TableHead';
 import TableRow from '@mui/material/TableRow';
 import Paper from '@mui/material/Paper';
 import Typography from '@mui/material/Typography';
+import Pagination from '@mui/material/Pagination';
+import Stack from '@mui/material/Stack';
+import { createTheme, ThemeProvider } from '@mui/material/styles';
 
 import api from '../../utils/api';
+
+const theme = createTheme({
+  palette: {
+    primary: {
+      main: '#f6c71e',
+    },
+  },
+});
 
 const Products = () => {
   const [products, setProducts] = useState([]);
   const { setFlashMessage } = useFlashMessage();
+  const [currentPage, setCurrentPage] = useState(0);
+  const perPage = 6;
+  const pageCount = Math.ceil(products.length / perPage);
 
   useEffect(() => {
     api.get('/products').then((response) => {
@@ -27,20 +41,44 @@ const Products = () => {
     });
   }, []);
 
+  const handlePageChange = (event, page) => {
+    setCurrentPage(page - 1);
+  };
+
+  const tableContainerStyle = {
+    margin: '20px',
+    border: '1px solid #ccc',
+    borderRadius: '5px',
+    padding: '10px',
+    width: '1200px',
+  };
+
+
+  const tableRowStyle = {
+    backgroundColor: 'lightyellow',
+  };
+
+  const paginationStyle = {
+    display: 'flex',
+    justifyContent: 'center',
+    alignItems: 'center',
+    marginTop: '20px',
+  };
+
+  const offset = currentPage * perPage;
+  const currentProducts = products.slice(offset, offset + perPage);
+
   return (
     <>
       <Box sx={{ display: 'flex' }}>
         <Box
           component="main"
-          sx={{ flexGrow: 1, p: 3, marginTop: '64px', marginLeft: '60px' }}
+          sx={{ flexGrow: 1, p: 3, marginTop: '0px', marginLeft: '60px' }}
         >
           <Sidebar />
-          <Typography variant="h1" component="h1" mb={3}>
-            Produtos
-          </Typography>
           <CreateProduct />
 
-          <TableContainer component={Paper}>
+          <TableContainer component={Paper} style={tableContainerStyle}>
             <Table>
               <TableHead>
                 <TableRow>
@@ -57,12 +95,10 @@ const Products = () => {
                 </TableRow>
               </TableHead>
               <TableBody>
-                {products.map((product, index) => (
+                {currentProducts.map((product, index) => (
                   <TableRow
                     key={product.code}
-                    style={{
-                      backgroundColor: index % 2 === 0 ? 'lightyellow' : 'white',
-                    }}
+                    style={index % 2 === 0 ? {} : { backgroundColor: 'lightyellow' }}
                   >
                     <TableCell>{product.code}</TableCell>
                     <TableCell>{product.description}</TableCell>
@@ -79,7 +115,21 @@ const Products = () => {
               </TableBody>
             </Table>
           </TableContainer>
-        </Box>
+
+          <div style={paginationStyle}>
+            <ThemeProvider theme={theme}>
+              <Stack spacing={2}>
+                <Pagination
+                  count={pageCount}
+                  page={currentPage + 1}
+                  onChange={handlePageChange}
+                  color="primary"
+                  size="large"
+                />
+              </Stack>
+            </ThemeProvider>
+          </div>
+        </Box>  
       </Box>
     </>
   );
